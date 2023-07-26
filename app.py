b495a458c4ca6a9173ad0af106fe1b8c79e6662c
farahfarzana <farahfafar@gmail.com>
2023-07-26 14:38:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def generate_graph_page():
                     y=df_grouped['Total Students'],
                     text=df_grouped['Total Students'],
                     textposition='auto',
-                    
+                    marker_color=[colors.get(status.lower(), 'gray') for status in df_grouped[column]],  # Use default color if not found
                 )
                 fig.add_trace(trace)
 
