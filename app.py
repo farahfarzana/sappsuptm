0d563bba7d0a072c18ebe41551c2443a35b00746
farahfarzana <farahfafar@gmail.com>
2023-07-26 09:29:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def generate_graph_page():
                             x=df_status[column],
                             y=df_status['Total Students'],
                             text=df_status['Total Students'],
-                            textposition='auto',
+                            textposition='potrait',
                             name=status,
                         )
                         fig.add_trace(trace)
