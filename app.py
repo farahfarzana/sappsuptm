b98f668fcc1e8f4196c643244d0844262f7e4f52
farahfarzana <farahfafar@gmail.com>
2023-07-26 10:18:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,19 +113,7 @@ def generate_graph_page():
                         barmode='stack',
                     )
                     cols[i % 2].plotly_chart(fig)
-        elif groupby_column == 'Status Risk':
-            # Display the selected graph
-            df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].count()
-            fig = px.bar(
-                df_grouped,
-                x=groupby_column,
-                y='Total Students',
-                color='Student',
-                color_continuous_scale=['red', 'yellow', 'green'],
-                template='plotly_white',
-                title=f'<b>Total Students by {groupby_column}</b>'
-            )
-            st.plotly_chart(fig)
+
         else:
             df_grouped = df.groupby(by=[groupby_column, 'Status Risk'], as_index=False)[output_columns].count()
             fig = go.Figure()
