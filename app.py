de8810f02d18681a6d8d1c55726d99ff32d8fa0f
farahfarzana <farahfafar@gmail.com>
2023-07-26 14:15:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,42 +88,56 @@ def generate_graph_page():
             # Display all 8 graphs in 2 columns, 4 rows
             cols = st.columns(2)
             columns = ['Gender', 'Sponsorship', 'GPASem1', 'GPASem2', 'GPASem3', 'GPASem4', 'CGPA', 'Status Risk']
-            
-            
+
+            # Handle the 'Status Risk' graph separately
+            if 'Status Risk' in columns:
+                column = 'Status Risk'
+                # For 'Status Risk', count the occurrences of each value
+                df_grouped = df[column].value_counts().reset_index()
+                df_grouped.columns = [column, 'Total Students']
+
+                fig = go.Figure()
+                trace = go.Bar(
+                    x=df_grouped[column],
+                    y=df_grouped['Total Students'],
+                    text=df_grouped['Total Students'],
+                    textposition='auto',
+                )
+                fig.add_trace(trace)
+
+                fig.update_layout(
+                    title=f'<b>Total Students by {column} - Bar Chart</b>',
+                    xaxis_title=column,
+                    yaxis_title='Total Students',
+                )
+                cols[0].plotly_chart(fig)
+
+                # Remove 'Status Risk' from the columns list
+                columns = [col for col in columns if col != 'Status Risk']
+
+            # Handle the other columns with stacked column charts
             for i, column in enumerate(columns):
                 if column != 'All':
                     df_grouped = df.groupby(by=[column, 'Status Risk'], as_index=False)[output_columns].count()
-                    fig = go.Figure()
 
-                    if column == 'Status Risk':
-                        # Use normal bar graph for 'Status Risk'
+                    fig = go.Figure()
+                    for status in df_grouped['Status Risk'].unique():
+                        df_status = df_grouped[df_grouped['Status Risk'] == status]
                         trace = go.Bar(
-                            x=df_grouped[column],
-                            y=df_grouped['Total Students'],
-                            text=df_grouped['Total Students'],
+                            x=df_status[column],
+                            y=df_status['Total Students'],
+                            text=df_status['Total Students'],
                             textposition='auto',
-                            marker_color=colors['Status Risk'],
+                            marker_color=colors[status],
+                            name=status,
                         )
                         fig.add_trace(trace)
-                    else:
-                        # Use stacked column graph for other columns
-                        for status in df_grouped['Status Risk'].unique():
-                            df_status = df_grouped[df_grouped['Status Risk'] == status]
-                            trace = go.Bar(
-                                x=df_status[column],
-                                y=df_status['Total Students'],
-                                text=df_status['Total Students'],
-                                textposition='auto',
-                                marker_color=colors[status],
-                                name=status,
-                            )
-                            fig.add_trace(trace)
 
                     fig.update_layout(
-                        title=f'<b>Total Students by {column} - {"Bar" if column == "Status Risk" else "Stacked Column"} Chart</b>',
+                        title=f'<b>Total Students by {column} - Stacked Column Chart</b>',
                         xaxis_title=column,
                         yaxis_title='Total Students',
-                        barmode='stack' if column != 'Status Risk' else None,
+                        barmode='stack',
                     )
                     cols[i % 2].plotly_chart(fig)
         elif groupby_column == 'Status Risk':
