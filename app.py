a0da8791cbe3034a2e8322e18cd96c940fa38c2c
farahfarzana <farahfafar@gmail.com>
2023-07-30 16:44:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -239,8 +239,8 @@ def generate_graph_page():
                 )
                 st.plotly_chart(fig)
 
-        except KeyError:
-            st.write("Error: The uploaded Excel file does not contain the required column. Please make sure it follows the correct format.")
+        except KeyError as e:
+            st.write("Error: The uploaded Excel file does not contain the required column. Please make sure it follows the correct format. Thank you. ")
         except Exception as e:
             st.write("Error: Unable to read the uploaded Excel file. Please make sure it follows the correct format.")
             st.write(e)
